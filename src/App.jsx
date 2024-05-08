be19c28a4821fbd9ca0a2a21c61789045e173383
tubisaffo <tubisaffo@gmail.com>
2024-05-08 06:56:48 +0300

    added searchBox

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const getMovieRequest = async () => {
-    const url =
-      "https://www.omdbapi.com/?i=guardian of the galaxy&apikey=9a9cb4d4";
+    const url = "https://www.omdbapi.com/?s=avengers&apikey=9a9cb4d4";
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -19,9 +20,14 @@ function App() {
     getMovieRequest();
   }, []);
   return (
-    <div>
-      <Header />
-      <MovieList movies={movies} />
+    <div className="App">
+      <div className="row">
+        <Header />
+        <SearchBox />
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
