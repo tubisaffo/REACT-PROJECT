fb86c29b91a9031b72fea6e439779985d1724dc6
tubisaffo <tubisaffo@gmail.com>
2024-05-09 10:13:49 +0300

    Project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,56 @@ import MovieList from "./components/MovieList";
 import SearchBox from "./components/SearchBox";
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([
+    {
+      Title: "Star Wars: Episode IV - A New Hope",
+      Year: "1977",
+      imdbID: "tt0076759",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode V - The Empire Strikes Back",
+      Year: "1980",
+      imdbID: "tt0080684",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VI - Return of the Jedi",
+      Year: "1983",
+      imdbID: "tt0086190",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VII - The Force Awakens",
+      Year: "2015",
+      imdbID: "tt2488496",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode I - The Phantom Menace",
+      Year: "1999",
+      imdbID: "tt0120915",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode III - Revenge of the Sith",
+      Year: "2005",
+      imdbID: "tt0121766",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg",
+    },
+  ]);
   const [searchValue, setSearchValue] = useState("");
 
   const getMovieRequest = async (searchValue) => {
