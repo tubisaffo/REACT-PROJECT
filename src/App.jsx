65581d3f99dd2837f58944f65646786598e30771
edwardkoikai <edwardkoikai@gmail.com>
2024-05-10 12:37:45 +0300

    Resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-=======
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import "./App.css";
@@ -47,7 +37,6 @@ function App() {
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
->>>>>>> Stashed changes
   return (
     <>
       <div>
